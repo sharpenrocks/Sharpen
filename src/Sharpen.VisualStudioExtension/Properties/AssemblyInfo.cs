33bfaaa64ecfcaa45aa87e523c2a23be3e2465d7
Igor Rončević <ironcev@hotmail.com>
2017-10-18 23:12:46 +0200

    feat: Adds empty Sharpen.VisualStudioExtension project

---
+++ src/Sharpen.VisualStudioExtension/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sharpen for Visual Studio")]
+[assembly: AssemblyDescription("Sharpen extension for Microsoft Visual Studio.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Igor Rončević")]
+[assembly: AssemblyProduct("Sharpen for Visual Studio")]
+[assembly: AssemblyCopyright("Copyright © 2017 Igor Rončević")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.1.0.*")]
+// We are not setting the AssemblyFileVersion explicitly. This will automatically make it same as the AssemblyVersion.
