8d47f009053e28542ed58dc561b0c399abc1f811
Igor Rončević <ironcev@hotmail.com>
2017-10-19 22:30:23 +0200

    feat: Adds empty Sharpen Whole Solution Command

---
+++ src/Sharpen.VisualStudioExtension/Commands/SharpenWholeSolutionCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.Design;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Sharpen.VisualStudioExtension.Commands
+{
+    internal sealed class SharpenWholeSolutionCommand
+    {
+        public const int CommandId = 0x0100;
+        public static readonly Guid CommandSet = new Guid("b5e43671-9e80-4169-beb1-74b688b21375");
+
+        private readonly Package package;
+
+        private SharpenWholeSolutionCommand(Package package)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+
+            if (ServiceProvider.GetService(typeof(IMenuCommandService)) is OleMenuCommandService commandService)
+            {
+                var menuCommandId = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(OnMenuItemClicked, menuCommandId);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        public static SharpenWholeSolutionCommand Instance { get; private set; }
+
+        private IServiceProvider ServiceProvider => package;
+
+        public static void Initialize(Package package)
+        {
+            Instance = new SharpenWholeSolutionCommand(package);
+        }
+
+        private void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            VsShellUtilities.ShowMessageBox(
+                ServiceProvider,
+                "Sharpen Whole Solution - coming soon :-)",
+                "Sharpen",
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
---
+++ src/Sharpen.VisualStudioExtension/SharpenPackage.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+using Sharpen.VisualStudioExtension.Commands;
+
+namespace Sharpen.VisualStudioExtension
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "0.1.0", IconResourceID = 400)] // Info on this package for Help/About.
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms.")]
+    public sealed class SharpenPackage : Package
+    {
+        public const string PackageGuidString = "01263ec2-7232-4367-a2cd-3982380b3985";
+
+        protected override void Initialize()
+        {
+            SharpenWholeSolutionCommand.Initialize(this);
+            base.Initialize();
+        }
+    }
+}
