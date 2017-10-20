90707319709f7e75885e2c95d28254db3c628ad0
Igor Rončević <ironcev@hotmail.com>
2017-10-20 21:13:01 +0200

    feat: Initially docks Sharpen Results tool window

--- src/Sharpen.VisualStudioExtension/SharpenPackage.cs
+++ src/Sharpen.VisualStudioExtension/SharpenPackage.cs
@@ -11,7 +11,7 @@ namespace Sharpen.VisualStudioExtension
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms.")]
-    [ProvideToolWindow(typeof(SharpenResultsToolWindow))]
+    [ProvideToolWindow(typeof(SharpenResultsToolWindow), Style = VsDockStyle.Tabbed, Window = "34E76E81-EE4A-11D0-AE2E-00A0C90FFFC3" /* Output Window. */)]
     public sealed class SharpenPackage : Package
     {
         public const string PackageGuidString = "01263ec2-7232-4367-a2cd-3982380b3985";
