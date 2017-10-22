fa6324e6ac4c838e37d33c62561e388bcdfe1ea6
Igor Rončević <ironcev@hotmail.com>
2017-10-22 22:57:43 +0200

    test: Adds smoke tests for UseExpressionBodyForConstructors

---
+++ tests/smoke/CSharp70/UseExpressionBodyForConstructors/ConstructorsThatAreCandidatesToHaveExpressionBody.cs
@@ -0,0 +1,65 @@
+﻿// ReSharper disable All
+
+using System;
+
+namespace CSharp70.UseExpressionBodyForConstructors
+{
+    public class ConstructorsThatAreCandidatesToHaveExpressionBody
+    {
+        public string S { get; }
+        private readonly int i;
+
+        public ConstructorsThatAreCandidatesToHaveExpressionBody()
+        {
+            Console.WriteLine(string.Empty);
+        }
+        public ConstructorsThatAreCandidatesToHaveExpressionBody(int i)
+        {
+            this.i = i;
+        }
+        public ConstructorsThatAreCandidatesToHaveExpressionBody(string s)
+        {
+            S = s;
+        }
+        public ConstructorsThatAreCandidatesToHaveExpressionBody(int i, string s)
+        {
+            S = s ?? throw new ArgumentNullException(nameof(s));
+        }
+        public ConstructorsThatAreCandidatesToHaveExpressionBody(int i, string s, double d) : this(i, s)
+        {
+            S = s ?? throw new ArgumentNullException(nameof(s));
+        }
+    }
+
+    public class ConstructorsThatAreCandidatesToHaveExpressionBodyWithComments
+    {
+        public string S { get; }
+        private readonly int i;
+
+        public ConstructorsThatAreCandidatesToHaveExpressionBodyWithComments()
+        {
+            // This is some comment.
+            Console.WriteLine(string.Empty);
+        }
+        public ConstructorsThatAreCandidatesToHaveExpressionBodyWithComments(int i)
+        {
+            // This is some comment.
+            this.i = i;
+        }
+        public ConstructorsThatAreCandidatesToHaveExpressionBodyWithComments(string s)
+        {
+            // This is some comment.
+            S = s;
+        }
+        public ConstructorsThatAreCandidatesToHaveExpressionBodyWithComments(int i, string s)
+        {
+            // This is some comment.
+            S = s ?? throw new ArgumentNullException(nameof(s));
+        }
+        public ConstructorsThatAreCandidatesToHaveExpressionBodyWithComments(int i, string s, double d) : this(i, s)
+        {
+            // This is some comment.
+            S = s ?? throw new ArgumentNullException(nameof(s));
+        }
+    }
+}
