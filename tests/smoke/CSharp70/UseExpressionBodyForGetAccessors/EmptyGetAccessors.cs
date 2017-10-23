9febc5690a075a6846146461feee3cc428358326
Igor Rončević <ironcev@hotmail.com>
2017-10-23 22:53:08 +0200

    test: Adds smoke tests for UseExpressionBodyForGetAccessors

---
+++ tests/smoke/CSharp70/UseExpressionBodyForGetAccessors/EmptyGetAccessors.cs
@@ -0,0 +1,24 @@
+﻿// ReSharper disable All
+
+namespace CSharp70.UseExpressionBodyForGetAccessors
+{
+    public class EmptyGetAccessors
+    {
+        public static int StaticIntProperty { get; }
+        public static string StaticStringProperty { get; set; }
+        public int IntProperty { get; }
+        public string StringProperty { get; set; }
+    }
+
+    public abstract class BaseClass
+    {
+    }
+
+    public class EmptyGetAccessorsWithBaseClass : BaseClass
+    {
+        public static int StaticIntProperty { get; }
+        public static string StaticStringProperty { get; set; }
+        public int IntProperty { get; }
+        public string StringProperty { get; set; }
+    }
+}
---
+++ tests/smoke/CSharp70/UseExpressionBodyForGetAccessors/GetAccessorsThatAreCandidatesToHaveExpressionBody.cs
@@ -0,0 +1,62 @@
+﻿// ReSharper disable All
+
+using System;
+
+namespace CSharp70.UseExpressionBodyForGetAccessors
+{
+    public class GetAccessorsThatAreCandidatesToHaveExpressionBody
+    {
+        public string S { get; }
+        private int i;
+
+        public int IntProperty
+        {
+            get { return i; }
+            set { }
+        }
+        public string StringProperty
+        {
+            get { return S ?? throw new ArgumentNullException(nameof(S)); }
+            set { }
+        }
+        public double DoubleProperty
+        {
+            get { return Convert.ToDouble(S.ToUpper().ToLower()); }
+            set { }
+        }
+    }
+
+    public class ConstructorsThatAreCandidatesToHaveExpressionBodyWithComments
+    {
+        public string S { get; }
+        private int i;
+
+        public int IntProperty
+        {
+            get
+            {
+                // This is some comment.
+                return i;
+            }
+            set { }
+        }
+        public string StringProperty
+        {
+            get
+            {
+                // This is some comment.
+                return S ?? throw new ArgumentNullException(nameof(S));
+            }
+            set { }
+        }
+        public double DoubleProperty
+        {
+            get
+            {
+                // This is some comment.
+                return Convert.ToDouble(S.ToUpper().ToLower());
+            }
+            set { }
+        }
+    }
+}
