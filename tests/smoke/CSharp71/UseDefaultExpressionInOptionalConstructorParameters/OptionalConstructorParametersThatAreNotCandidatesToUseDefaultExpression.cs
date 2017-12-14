0bfdfacdd4bbd80e022e0020f3af98b387dcc528
Igor Rončević <ironcev@hotmail.com>
2017-12-14 11:39:30 +0100

    test: Adds negative smoke tests for default expression in parameters

---
+++ tests/smoke/CSharp71/UseDefaultExpressionInOptionalConstructorParameters/OptionalConstructorParametersThatAreNotCandidatesToUseDefaultExpression.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable All
+
+using System.Collections;
+
+namespace CSharp71.UseDefaultExpressionInOptionalConstructorParameters
+{
+    public class OptionalConstructorParametersThatAreNotCandidatesToUseDefaultExpression
+    {
+        OptionalConstructorParametersThatAreNotCandidatesToUseDefaultExpression(int i, int p = default(short)) { }
+        OptionalConstructorParametersThatAreNotCandidatesToUseDefaultExpression(string s, System.Int32 p = default(short)) { }
+        OptionalConstructorParametersThatAreNotCandidatesToUseDefaultExpression(double d, int p = default(System.Int16)) { }
+
+        OptionalConstructorParametersThatAreNotCandidatesToUseDefaultExpression(int i, IEnumerable p = default(IList)) { }
+        OptionalConstructorParametersThatAreNotCandidatesToUseDefaultExpression(string s, System.Collections.IEnumerable p = default(IList)) { }
+        OptionalConstructorParametersThatAreNotCandidatesToUseDefaultExpression(double d, IEnumerable p = default(System.Collections.IList)) { }
+    }
+}
