019689f6944f1deeed2cc328727ff3a10849f2e6
Igor Rončević <ironcev@hotmail.com>
2018-10-06 22:51:17 +0200

    perf: Optimizes DisplayText

--- src/Sharpen.Engine/DisplayText.cs
+++ src/Sharpen.Engine/DisplayText.cs
@@ -2,6 +2,7 @@
 using System;
 #endif
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Sharpen.Engine
@@ -10,24 +11,24 @@ namespace Sharpen.Engine
     {
         public static string For(SyntaxNode syntaxNode)
         {
-            switch (syntaxNode)
+            switch (syntaxNode.Kind())
             {
-                case DestructorDeclarationSyntax destructor: return For(destructor);
-                case ConstructorDeclarationSyntax constructor: return For(constructor);
-                case PropertyDeclarationSyntax property: return For(property);
-                case IndexerDeclarationSyntax indexer: return For(indexer);
-                case ReturnStatementSyntax @return: return For(@return);
-                case ParameterSyntax parameter: return For(parameter);
-                case ArgumentSyntax argument: return For(argument);
-                case InvocationExpressionSyntax invocation: return For(invocation);
-                case LocalFunctionStatementSyntax localFunction: return For(localFunction);
-                case ObjectCreationExpressionSyntax objectCreation: return For(objectCreation);
-                case MemberAccessExpressionSyntax memberAccess: return For(memberAccess);
-                // In general, we want to have a nice customized representation for each
+                case SyntaxKind.DestructorDeclaration: return For((DestructorDeclarationSyntax)syntaxNode);
+                case SyntaxKind.ConstructorDeclaration: return For((ConstructorDeclarationSyntax)syntaxNode);
+                case SyntaxKind.PropertyDeclaration: return For((PropertyDeclarationSyntax)syntaxNode);
+                case SyntaxKind.IndexerDeclaration: return For((IndexerDeclarationSyntax)syntaxNode);
+                case SyntaxKind.ReturnStatement: return syntaxNode.ToString();
+                case SyntaxKind.Parameter: return syntaxNode.ToString();
+                case SyntaxKind.Argument: return syntaxNode.ToString();
+                case SyntaxKind.InvocationExpression: return syntaxNode.ToString();
+                case SyntaxKind.LocalFunctionStatement: return For((LocalFunctionStatementSyntax)syntaxNode);
+                case SyntaxKind.ObjectCreationExpression: return syntaxNode.ToString();
+                case SyntaxKind.SimpleMemberAccessExpression: return syntaxNode.ToString();
+                // In general, we want to have a proper customized representation for each
                 // of the known nodes. Still, since sometimes we do display arbitrary nodes
-                // we will need the fallback to SyntaxNode in the release build.
+                // we will need the fallback to generic display in the release build.
                 // During debugging, let it crash so that we can extend the list with additional
-                // known cases.
+                // known cases, even if we will choose that they have a generic display.
                 #if DEBUG
                 default:
                     throw new ArgumentOutOfRangeException
@@ -36,82 +37,39 @@ namespace Sharpen.Engine
                         $"Getting the display text for the syntax node of type {syntaxNode.GetType().Name} is currently not supported."
                     );
                 #else
-                default: return FallbackFor(syntaxNode);
+                default: return syntaxNode.ToString();
                 #endif
             }
         }
 
-        #if !DEBUG
-        private static string FallbackFor(SyntaxNode syntaxNode)
-        {
-            return syntaxNode.ToString();
-        }
-        #endif
-
         private static string For(DestructorDeclarationSyntax destructor)
         {
             destructor = destructor.WithoutLeadingTrivia();
-            return $"{destructor.Modifiers.ToFullString()}{destructor.TildeToken.ToFullString()}{destructor.Identifier.ToFullString()}{destructor.ParameterList.ToFullString()}".Trim();
+            return $"{destructor.Modifiers.ToFullString()}{destructor.TildeToken.ToFullString()}{destructor.Identifier.ToFullString()}{destructor.ParameterList}";
         }
 
         private static string For(ConstructorDeclarationSyntax constructor)
         {
             constructor = constructor.WithoutLeadingTrivia();
-            return $"{constructor.Modifiers.ToFullString()}{constructor.Identifier.ToFullString()}{constructor.ParameterList.ToFullString()}".Trim();
+            return $"{constructor.Modifiers.ToFullString()}{constructor.Identifier.ToFullString()}{constructor.ParameterList}";
         }
 
         private static string For(PropertyDeclarationSyntax property)
         {
             property = property.WithoutLeadingTrivia();
-            return $"{property.Modifiers.ToFullString()}{property.Type.ToFullString()}{property.Identifier.ToFullString()}".Trim();
+            return $"{property.Modifiers.ToFullString()}{property.Type.ToFullString()}{property.Identifier}";
         }
 
         private static string For(IndexerDeclarationSyntax indexer)
         {
             indexer = indexer.WithoutLeadingTrivia();
-            return $"{indexer.Modifiers.ToFullString()}{indexer.Type.ToFullString()}{indexer.ThisKeyword.ToFullString()}{indexer.ParameterList.ToFullString()}".Trim();
-        }
-
-        private static string For(ReturnStatementSyntax @return)
-        {
-            @return = @return.WithoutLeadingTrivia();
-            return $"{@return.ToFullString()}".Trim();
-        }
-
-        private static string For(ParameterSyntax parameter)
-        {
-            parameter = parameter.WithoutLeadingTrivia();
-            return $"{parameter.ToFullString()}".Trim();
-        }
-
-        private static string For(ArgumentSyntax argument)
-        {
-            argument = argument.WithoutLeadingTrivia();
-            return $"{argument.ToFullString()}".Trim();
-        }
-
-        private static string For(InvocationExpressionSyntax invocation)
-        {
-            invocation = invocation.WithoutLeadingTrivia();
-            return $"{invocation.ToFullString()}".Trim();
+            return $"{indexer.Modifiers.ToFullString()}{indexer.Type.ToFullString()}{indexer.ThisKeyword.ToFullString()}{indexer.ParameterList}";
         }
 
         private static string For(LocalFunctionStatementSyntax localFunction)
         {
             localFunction = localFunction.WithoutLeadingTrivia();
-            return $"{localFunction.Modifiers.ToFullString()}{localFunction.Identifier.ToFullString()}{localFunction.ParameterList.ToFullString()}".Trim();
-        }
-
-        private static string For(ObjectCreationExpressionSyntax objectCreation)
-        {
-            objectCreation = objectCreation.WithoutLeadingTrivia();
-            return $"{objectCreation.ToFullString()}".Trim();
-        }
-
-        private static string For(MemberAccessExpressionSyntax memberAccess)
-        {
-            memberAccess = memberAccess.WithoutLeadingTrivia();
-            return $"{memberAccess.ToFullString()}".Trim();
+            return $"{localFunction.Modifiers.ToFullString()}{localFunction.Identifier.ToFullString()}{localFunction.ParameterList}";
         }
     }
 }
