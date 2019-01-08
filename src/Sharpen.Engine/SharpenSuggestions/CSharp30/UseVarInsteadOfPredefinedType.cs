8cb56153195a342db9ae17ca7fc7f037d4002b07
shankyjain7243 <shanky.jain@ymail.com>
2019-01-08 14:07:36 +0530

    Sharpen now reports use of var in case of Implicitly Typed Local Variables

--- src/Sharpen.Engine/SharpenSuggestions/CSharp30/UseVarInsteadOfPredefinedType.cs
+++ src/Sharpen.Engine/SharpenSuggestions/CSharp30/UseVarInsteadOfPredefinedType.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Sharpen.Engine.Analysis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+
+
 
 namespace Sharpen.Engine.SharpenSuggestions.CSharp30
 {
@@ -13,22 +17,10 @@ namespace Sharpen.Engine.SharpenSuggestions.CSharp30
 
         public ICSharpFeature LanguageFeature => CSharpFeatures.ImplicitlyTypedLocalVaraiables.Instance;
 
-        public string FriendlyName => "Consider declaring variable as var";
+        public string FriendlyName => "Use var keyword in variable declaration with object creation";
 
         public IEnumerable<AnalysisResult> Analyze(SyntaxTree syntaxTree, SemanticModel semanticModel, SingleSyntaxTreeAnalysisContext analysisContext)
         {
-            var analysisResult = new List<AnalysisResult>();
-            analysisResult.AddRange(GetVariableAnalysisResult(syntaxTree, analysisContext));
-            return analysisResult;
-        }
-
-       
-        public static readonly UseVarInsteadOfPredefinedType Instance = new UseVarInsteadOfPredefinedType();
-
-
-        private List<AnalysisResult> GetVariableAnalysisResult(SyntaxTree syntaxTree, SingleSyntaxTreeAnalysisContext analysisContext)
-        {
-
             var analysisResult = new List<AnalysisResult>();
 
             var variableDeclarations = syntaxTree.GetRoot().DescendantNodes()
@@ -36,47 +28,59 @@ namespace Sharpen.Engine.SharpenSuggestions.CSharp30
 
             foreach (var declaration in variableDeclarations)
             {
-                if (declaration.ChildNodes().Any(x => (x is GenericNameSyntax)) || declaration.ChildNodes().Any(x => (x is QualifiedNameSyntax)))
+
+                var LHSType = GetLeftSideCLRType(declaration)?.ToLower();
+                var RHSType = GetRightSideCLRType(declaration)?.ToLower();
+
+                if ((LHSType != null && RHSType != null) && (RHSType.Contains(LHSType) || LHSType.Contains(RHSType)))
                 {
-                    var LHSType = declaration.DescendantNodes().OfType<GenericNameSyntax>().FirstOrDefault();
-                    if (LHSType != null)
-                    {
-                        var RHSType = declaration.DescendantNodes().OfType<ObjectCreationExpressionSyntax>()
-                                .FirstOrDefault().DescendantNodes().OfType<GenericNameSyntax>().FirstOrDefault();
-                        if (RHSType != null && (LHSType.ToString() == RHSType.ToString()))
-                        {
-                            analysisResult.Add(new AnalysisResult(
-                               this,
-                               analysisContext,
-                               syntaxTree.FilePath,
-                               declaration.GetFirstToken(),
-                               declaration));
-                        }
-                    }
+                    analysisResult.Add(new AnalysisResult(
+                            this,
+                            analysisContext,
+                            syntaxTree.FilePath,
+                            declaration.GetFirstToken(),
+                            declaration));
                 }
-
-
-                if ((declaration.ChildNodes().Any(x => x is PredefinedTypeSyntax) || declaration.ChildNodes().Any(x => x is IdentifierNameSyntax))
-                    && declaration.DescendantNodes().Any(x => x is EqualsValueClauseSyntax))
-                {
-                    //var LHSType = declaration.ChildNodes().FirstOrDefault(x => x is PredefinedTypeSyntax);
-                    //var RHSType = declaration.DescendantNodes().FirstOrDefault(x => x is ObjectCreationExpressionSyntax).ChildNodes().FirstOrDefault(x => x is PredefinedTypeSyntax);
-                    //if(LHSType != null && RHSType != null && RHSType.ToString() == LHSType.ToString())
-                    //{
-                        analysisResult.Add(new AnalysisResult(
-                              this,
-                              analysisContext,
-                              syntaxTree.FilePath,
-                              declaration.GetFirstToken(),
-                              declaration));
-
-                   // }
-
-                }
-
-
             }
             return analysisResult;
         }
+
+       
+        public static readonly UseVarInsteadOfPredefinedType Instance = new UseVarInsteadOfPredefinedType();
+
+
+      
+
+        private string GetRightSideCLRType(VariableDeclarationSyntax declaration)
+        {
+            int totalDeclarationsInLine =  declaration.DescendantNodes().Count(x => x is VariableDeclaratorSyntax);
+
+            if(totalDeclarationsInLine > 1)
+            {
+                return null;
+            }
+
+            return declaration.DescendantNodes().FirstOrDefault(x => x is ObjectCreationExpressionSyntax).ChildNodes()?
+                                       .FirstOrDefault(x => x is QualifiedNameSyntax || x is GenericNameSyntax || x is PredefinedTypeSyntax || x is IdentifierNameSyntax)?
+                                       .DescendantTokens()?.Select(x => x.ValueText)?
+                                       .Aggregate((i,j) => i + j);
+
+            
+
+
+        }
+
+        private string GetLeftSideCLRType(VariableDeclarationSyntax declaration)
+        {
+            return declaration.ChildNodes()?
+                .FirstOrDefault(x => x is PredefinedTypeSyntax || x is GenericNameSyntax || x is QualifiedNameSyntax || x is IdentifierNameSyntax)?
+                .DescendantTokens()?.Select(x => x.ValueText)?
+                .Aggregate((i, j) => i + j);
+        }
     }
+
+
+
+
+   
 }
