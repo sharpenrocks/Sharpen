f68c9bec2af99231cb18684c418a30973a24cfea
Igor Rončević <ironcev@hotmail.com>
2019-05-03 23:19:30 +0200

    test: Strengthens UseVarKeyword tests with expected display text

--- src/Sharpen.Engine.Tests.Unit/SharpenSuggestions/CSharp30/ImplicitlyTypedLocalVariables/UseVarKeywordInVariableDeclarationWithObjectCreationTests.cs
+++ src/Sharpen.Engine.Tests.Unit/SharpenSuggestions/CSharp30/ImplicitlyTypedLocalVariables/UseVarKeywordInVariableDeclarationWithObjectCreationTests.cs
@@ -19,25 +19,32 @@ namespace Sharpen.Engine.Tests.Unit.SharpenSuggestions.CSharp30.ImplicitlyTypedL
             nameof(UseVarKeywordInVariableDeclarationWithObjectCreation)
         );
 
-
-        [TestCase("int i = new System.Int32();")]
-        [TestCase("List<int> list = new List<int>(10000);")]
-        [TestCase("List<int> list = new System.Collections.Generic.List<int>(10000);")]
-        [TestCase("System.Collections.Generic.List<int> list = new List<int>(10000);")]
-        [TestCase("CustomClass myObject = new CustomClass(1,2);")]
-        [TestCase("CustomClass myObject1 = new CustomClass { MyProperty = 4 };")]
-        [TestCase(@"using (StreamReader file = new StreamReader(""C:\\myfile.txt"")) {}")]
-        public void Left_side_type_exactly_the_same_as_right_hand_type__analysis_gives_suggestion(string innerCode)
+        [TestCase("int i = new System.Int32();", "int i = new System.Int32()")]
+        [TestCase("List<int> list = new List<int>(10000);", "List<int> list = new List<int>(10000)")]
+        [TestCase("List<int> list = new System.Collections.Generic.List<int>(10000);", "List<int> list = new System.Collections.Generic.List<int>(10000)")]
+        [TestCase("System.Collections.Generic.List<int> list = new List<int>(10000);", "System.Collections.Generic.List<int> list = new List<int>(10000)")]
+        [TestCase("CustomClass myObject = new CustomClass(1,2);", "CustomClass myObject = new CustomClass(1,2)")]
+        [TestCase("CustomClass myObject1 = new CustomClass { MyProperty = 4 };", "CustomClass myObject1 = new CustomClass { MyProperty = 4 }")]
+        [TestCase(@"using (StreamReader file = new StreamReader(""C:\\myfile.txt"")) {}", @"StreamReader file = new StreamReader(""C:\\myfile.txt"")")]
+        public void Left_side_type_exactly_the_same_as_right_hand_type__analysis_gives_suggestion(string innerCode, string expectedDisplayText)
         {
             var analysisResult = Analyze(innerCode);
 
-            Assert.That(analysisResult.Length, Is.EqualTo(1));
+            Assert.That(analysisResult.Length, Is.EqualTo(1), innerCode);
+            Assert.That(analysisResult[0].DisplayText, Is.EqualTo(expectedDisplayText));
         }
 
         [TestCase("IEnumerable<int> list = new List<int>(10000);")]
         [TestCase("long l = (long)(new int());")]
-        [TestCase("List<int> first = new List<int>(), second = new List<int>();")]
         public void Left_side_type_not_exactly_the_same_as_right_hand_type__analysis_does_not_give_suggestion(string innerCode)
+        {
+            var analysisResult = Analyze(innerCode);
+
+            Assert.That(analysisResult.Length, Is.EqualTo(0));
+        }
+
+        [TestCase("List<int> first = new List<int>(), second = new List<int>();")]
+        public void Multiple_variables_declared_in_a_single_declaration__analysis_does_not_give_suggestion(string innerCode)
         {
             var analysisResult = Analyze(innerCode);
 
