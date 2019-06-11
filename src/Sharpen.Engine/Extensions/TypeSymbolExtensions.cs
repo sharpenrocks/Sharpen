4ee8fb253f755e1c29bd4eb0ed2f32738b9cdcf0
Igor Rončević <ironcev@hotmail.com>
2019-06-11 22:05:53 +0430

    feat: Enable nullable context recognizes already nullable identifiers

---
+++ src/Sharpen.Engine/Extensions/TypeSymbolExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Sharpen.Engine.Extensions
+{
+    internal static class TypeSymbolExtensions
+    {
+        public static bool IsUnconstrainedTypeParameter(this ITypeSymbol typeSymbol)
+        {
+            return typeSymbol.TypeKind == TypeKind.TypeParameter &&
+                   // For unconstrained type parameters both IsValueType
+                   // and IsReferenceType are false.
+                   typeSymbol.IsValueType == false &&
+                   typeSymbol.IsReferenceType == false;
+        }
+    }
+}
