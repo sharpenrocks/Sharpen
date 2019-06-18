210df6c16fc13428627fb9bbb0a7be4920297835
Igor Rončević <ironcev@hotmail.com>
2019-06-18 12:42:02 +0430

    test: Adds negative smoke tests for nullable fields and properties
    If they are not declared in code.

---
+++ tests/smoke/CSharp80/NullableReferenceTypes/EnableNullableContextAndDeclareFieldAsNullable/ClassFieldCannotBeDeclaredAsNullableNotDeclaredInCode.cs
@@ -0,0 +1,23 @@
+﻿// ReSharper disable All
+
+namespace CSharp80.NullableReferenceTypes.EnableNullableContextAndDeclareFieldAsNullable
+{
+    public class ClassFieldCannotBeDeclaredAsNullableNotDeclaredInCode
+    {
+        public void FieldIsNullableForDifferentReasons()
+        {
+            string dummy;
+
+            if (string.Empty == null) return;
+            if (null == string.Empty) return;            
+            if (string.Empty != null) return;
+            if (null != string.Empty) return;
+            if (string.Empty == null) return;
+            if (null == string.Empty) return;
+
+            string.Empty?.Length.ToString();
+
+            dummy = string.Empty ?? string.Empty;
+        }
+    }
+}
---
+++ tests/smoke/CSharp80/NullableReferenceTypes/EnableNullableContextAndDeclarePropertyAsNullable/ClassPropertyCannotBeDeclaredAsNullableNotDeclaredInCode.cs
@@ -0,0 +1,28 @@
+﻿// ReSharper disable All
+
+using System;
+using System.Net.Http;
+
+namespace CSharp80.NullableReferenceTypes.EnableNullableContextAndDeclarePropertyAsNullable
+{
+    public class ClassPropertyCannotBeDeclaredAsNullableNotDeclaredInCode
+    {
+        public void PropertyIsNullableForDifferentReasons()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            httpClient.BaseAddress = null;
+
+            if (httpClient.BaseAddress == null) return;
+            if (null == httpClient.BaseAddress) return;            
+            if (httpClient.BaseAddress != null) return;
+            if (null != httpClient.BaseAddress) return;
+            if (httpClient.BaseAddress == null) return;
+            if (null == httpClient.BaseAddress) return;
+
+            httpClient.BaseAddress?.ToString();
+
+            var dummy = httpClient.BaseAddress ?? new Uri("");
+        }
+    }
+}
