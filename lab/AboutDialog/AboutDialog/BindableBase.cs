11a3391f151fa4f0f38272772fe108da2633e194
Andrija Frlan <andrija.frlan@netcom.hr>
2020-11-17 09:00:11 +0100

    style: Adds source comment, removes XML comments identation.

--- lab/AboutDialog/AboutDialog/BindableBase.cs
+++ lab/AboutDialog/AboutDialog/BindableBase.cs
@@ -1,33 +1,35 @@
-﻿using System.ComponentModel;
+﻿// The BindableBase code is copied, with minor changes, from https://gist.github.com/DanRigby/98fd60dd32df82a678cd
+
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace Sharpen
 {
     /// <summary>
-    ///     Implementation of <see cref="INotifyPropertyChanged" /> to simplify models.
+    /// Implementation of <see cref="INotifyPropertyChanged" /> to simplify models.
     /// </summary>
     public abstract class BindableBase : INotifyPropertyChanged
     {
         /// <summary>
-        ///     Multicast event for property change notifications.
+        /// Multicast event for property change notifications.
         /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
         /// <summary>
-        ///     Checks if a property already matches a desired value.  Sets the property and
-        ///     notifies listeners only when necessary.
+        /// Checks if a property already matches a desired value.  Sets the property and
+        /// notifies listeners only when necessary.
         /// </summary>
         /// <typeparam name="T">Type of the property.</typeparam>
         /// <param name="storage">Reference to a property with both getter and setter.</param>
         /// <param name="value">Desired value for the property.</param>
         /// <param name="propertyName">
-        ///     Name of the property used to notify listeners.  This
-        ///     value is optional and can be provided automatically when invoked from compilers that
-        ///     support CallerMemberName.
+        /// Name of the property used to notify listeners.  This
+        /// value is optional and can be provided automatically when invoked from compilers that
+        /// support CallerMemberName.
         /// </param>
         /// <returns>
-        ///     True if the value was changed, false if the existing value matched the
-        ///     desired value.
+        /// True if the value was changed, false if the existing value matched the
+        /// desired value.
         /// </returns>
         protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
         {
@@ -42,12 +44,12 @@ namespace Sharpen
         }
 
         /// <summary>
-        ///     Notifies listeners that a property value has changed.
+        /// Notifies listeners that a property value has changed.
         /// </summary>
         /// <param name="propertyName">
-        ///     Name of the property used to notify listeners.  This
-        ///     value is optional and can be provided automatically when invoked from compilers
-        ///     that support <see cref="CallerMemberNameAttribute" />.
+        /// Name of the property used to notify listeners.  This
+        /// value is optional and can be provided automatically when invoked from compilers
+        /// that support <see cref="CallerMemberNameAttribute" />.
         /// </param>
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
